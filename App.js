cc6cbedcec2657b9fc062ca1c4e96bed5be6147c
Gigi (gisela) <gisela.calva.241@my.csun.edu>
2023-07-17 13:53:09 -0700

    Gisela's new changes

--- App.js
+++ App.js
@@ -9,59 +9,115 @@ const CHATBOT_USER_OBJ = {
 
 export default function App() {
   const [messages, setMessages] = useState([]);
-
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+ 
   useEffect(() => {
-    setMessages([
-      {
+  setMessages([
+  {
         _id: 1,
         text: "Hello, welcome to simple trivia! Say 'Yes' when you're ready to play!",
         createdAt: new Date(),
         user: CHATBOT_USER_OBJ,
-      },
-    ]);
-  }, []);
+        },
+      ]);
+    }, 
+    []);
 
-  const addNewMessage = (newMessages) => {
+    const addNewMessage = (newMessages) => {
     setMessages((previousMessages) => {
-      // console.log("PREVIOUS MESSAGES:", previousMessages);
-      // console.log("NEW MESSAGE:", newMessages);
-      return GiftedChat.append(previousMessages, newMessages);
+    // console.log("PREVIOUS MESSAGES:", previousMessages);
+    // console.log("NEW MESSAGE:", newMessages);
+    return GiftedChat.append(previousMessages, newMessages);
     });
   };
 
-  const addBotMessage = (text) => {
+    const addBotMessage = (text) => {
+    console.log(text, currentQuestionIndex)
     addNewMessage([
-      {
-        _id: Math.round(Math.random() * 1000000),
-        text: text,
-        createdAt: new Date(),
-        user: CHATBOT_USER_OBJ,
-      },
-    ]);
-  };
+    {
+      _id: Math.round(Math.random() * 1000000),
+      text: text,
+      createdAt: new Date(),
+      user: CHATBOT_USER_OBJ,
+    },
+  ]);
+};
 
-  const respondToUser = (userMessages) => {
-    console.log("User message text:", userMessages[0].text);
+     const questions = [
+    {
+        id: 1,
+          text: "Which Disney princess has a pet tiger named Rajah?",
+          answer: "Jasmine",
+    },
+    {
+      id: 2,
+      text: "Great, what food am i currently craving?",
+      answer: "sushi",
+    },
+    {
+      id: 3,
+      text: "What is my favorite color?",
+      answer: "",
+    },
+    {
+      id: 4,
+      text: "What is the name of the street where Aladdin lives?",
+      answer: "Agrabah",
+    },
+    {
+      id: 5,
+      text: "Who is Simba's wise and trusted advisor in 'The Lion King'?",
+      answer: "Rafiki",
+    },
+];
 
-    // Simple chatbot logic (aka Checkpoint 2 onwards) here!
 
-    addBotMessage("I am da response!");
-  };
+    const respondToUser = (userMessages) => {
+    const userMessageText = userMessages[0].text;
 
-  const onSend = useCallback((messages = []) => {
+    console.log(userMessages[0].text, currentQuestionIndex)
+
+    if (userMessageText === "yes"||userMessageText === "Yes") {
+    const currentQuestion = questions[currentQuestionIndex];
+    addBotMessage(currentQuestion.text);
+    // } else {
+    //   addBotMessage("Please type 'Yes' to start.");
+  }
+
+    else if (userMessageText === questions[currentQuestionIndex].answer) {
+    addBotMessage("That's correct!");
+
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+    console.log("set current")
+
+    if (currentQuestionIndex + 1 < questions.length) {
+      const nextQuestion = questions[currentQuestionIndex + 1];
+      addBotMessage(nextQuestion.text);
+    } 
+  }
+    else {
+      addBotMessage("Please type 'Yes' to start.");
+    //   addBotMessage("Trivia completed!");
+    }
+};
+
+
+    const onSend = useCallback((messages = []) => {
     addNewMessage(messages);
-    // setTimeout(() => respondToUser(messages), 1000);
-  }, []);
+    }, []);
 
-  return (
+    return (
     <GiftedChat
-      messages={messages}
-      onSend={(messages) => onSend(messages)}
-      user={{
-        _id: 1,
-        name: "Baker",
-      }}
+    messages={messages}
+    onSend={(messages) => {
+    onSend(messages)
+    setTimeout(() => respondToUser(messages), 1000);
+    }}
+    user={{
+          _id: 1,
+          name: "Baker",
+    }}
       renderUsernameOnMessage={true}
-    />
-  );
+  />
+);
 }
